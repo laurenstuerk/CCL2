3ec1e83a60a9e2665e9d21691bda28a40ab36b8e
Laurens Türk <laurenstuerk@gmail.com>
2025-05-31 17:02:33 +0200

    feat: integrate Google OAuth for login and update dependencies
    - Added Google OAuth support in the LoginPage component.
    - Updated package.json and package-lock.json to include new dependencies: @react-oauth/google and @tanstack/react-query.
    - Removed ProfileLoader component as it is no longer needed.
    - Commented out the Footer component in App.jsx.
    - Cleaned up LoginPage component structure and added a Google login button.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,7 +68,7 @@ export default function App() {
 
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,18 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {AuthProvider} from './context/AuthContext.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <AuthProvider>
-            <App/>
+            <GoogleOAuthProvider clientId={CLIENT_ID}>
+                <App/>
+            </GoogleOAuthProvider>
         </AuthProvider>
-    </StrictMode>,)
+    </StrictMode>,
+)
